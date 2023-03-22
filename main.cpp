d9a26236134e7ec7da77b4481a86ab5edb97adf9
zayipupr <128625941+zayipupr@users.noreply.github.com>
2023-03-22 14:03:37 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  zayisHelloWorld
+//
+//  Created by Zayira Jordan on 3/22/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // this is zayis hello world code demo file
+    cout << "Hello, World!\n";
+    cout << "I am Zayi's Hello World!"<<endl;
+    
+    return 0;
+}
